2b3d7d5508b05d3e2284b7656050ba92182e6d3b
hesham <hesham@DESKTOP-ORE0907.localdomain>
2025-06-03 19:15:05 +0300

    list iam users

---
+++ boto3/first-boto3.py
@@ -0,0 +1,7 @@
+# This scrip lists of all iam users 
+import boto3    
+aws_management_console = boto3.session.Session(profile_name="default")
+iam_console = aws_management_console.resource("iam")
+
+for each_user in iam_console.users.all():
+    print(each_user.name)
---
+++ hello-world/hello.py
@@ -0,0 +1,10 @@
+
+print("Hello World")
+print ("what is your name ?") # ask for their name
+myname=input()
+print("it is good to meet you "+myname)
+print("the length of your name is :")
+print("len(myname)")
+print("what is your age?")#ask for thier age
+myage=input()
+print("you will be "+ str(int(myage)+1))
